fec931b703fe632ae1c5e75c9249723fd0526b5f
xuhongbin <xuhongbin@yy.com>
2022-07-13 11:39:07 +0800

    test: up

--- projects/emp-zustand/emp-config.js
+++ projects/emp-zustand/emp-config.js
@@ -1,10 +1,8 @@
 const {defineConfig} = require('@efox/emp')
-const path = require('path')
 module.exports = defineConfig(() => {
   return {
     moduleTransform: {
-      include: [require.resolve('zustand')],
-      // include: [/zustand/],
+      include: [/zustand/],
     },
     html: {
       title: 'EMP Zustand',
