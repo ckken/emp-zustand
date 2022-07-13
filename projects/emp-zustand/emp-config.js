4b9a029ebb7ca7531b1728b7930a48c988484b06
xuhongbin <xuhongbin@yy.com>
2022-07-13 10:19:37 +0800

    test: include node modules

--- projects/emp-zustand/emp-config.js
+++ projects/emp-zustand/emp-config.js
@@ -2,7 +2,8 @@ const {defineConfig} = require('@efox/emp')
 module.exports = defineConfig(() => {
   return {
     moduleTransform: {
-      include: ['zustand'],
+      // include: ['zustand'],
+      include: [/node_modules/],
     },
     html: {
       title: 'EMP Zustand',
