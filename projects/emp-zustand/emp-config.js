340a27943756d694d22a1f4b8accd41fef9303bc
xuhongbin <xuhongbin@yy.com>
2022-12-12 18:00:01 +0800

    feat: add vconsole

--- projects/emp-zustand/emp-config.js
+++ projects/emp-zustand/emp-config.js
@@ -12,6 +12,12 @@ module.exports = defineConfig(() => {
     },
     html: {
       title: 'EMP Zustand',
+			tags:{
+				headTags:[`<script src="https://unpkg.com/vconsole@latest/dist/vconsole.min.js"></script>
+				<script>
+					var vConsole = new window.VConsole();
+				</script>`]
+			}
     },
   }
 })
--- projects/emp-zustand/src/bootstrap.tsx
+++ projects/emp-zustand/src/bootstrap.tsx
@@ -1,10 +1,7 @@
 import {createRoot} from 'react-dom/client'
 import {RouterDom} from 'src/router'
-import {StrictMode} from 'react'
 const container = document.getElementById('emp-root') as HTMLElement
 const root = createRoot(container)
 root.render(
-  <StrictMode>
     <RouterDom />
-  </StrictMode>,
 )
