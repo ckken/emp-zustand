339aa3b3956311dafac3ff8643f670df18c01dd1
xuhongbin <xuhongbin@yy.com>
2023-09-22 11:53:47 +0800

    feat: add more case

--- projects/emp-valtio/src/App.tsx
+++ projects/emp-valtio/src/App.tsx
@@ -1,33 +1,126 @@
 import {Refresh} from './Refresh'
+import './style.css'
+const waitAsync = (time = 1000) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, time)
+  })
+}
 //
 import {proxy, useSnapshot} from 'valtio'
 const state = proxy({
   dur: 4,
-  count: 216,
+  config: {
+    count: 1,
+    info: 'count',
+  },
+  incDur() {
+    ++state.dur
+  },
+  decDur() {
+    --state.dur
+  },
+  async setCountAsync() {
+    await waitAsync(500)
+    state.config.count++
+  },
+  changeConfigInfo() {
+    state.config.info = `${Math.round(Math.random() * 100000)}`
+  },
 })
-const incDur = () => {
-  ++state.dur
-}
-const decDur = () => {
-  --state.dur
-}
+
 const ShowwDur = () => {
   const snap = useSnapshot(state)
   return (
-    <h1>
-      Snap Count: {snap.count} <Refresh />
-    </h1>
+    <>
+      <h1>
+        Snap Dur: {snap.dur} <Refresh />
+      </h1>
+    </>
+  )
+}
+const Control = () => {
+  const snap = useSnapshot(state)
+  return (
+    <>
+      <button className="btn" onClick={snap.decDur}>
+        -
+      </button>
+      <button className="btn" onClick={snap.incDur}>
+        +
+      </button>
+      <button className="btn" onClick={snap.setCountAsync}>
+        Count Async Plus
+      </button>
+      <button className="btn" onClick={snap.changeConfigInfo}>
+        Change Config Info
+      </button>
+      <Refresh />
+    </>
+  )
+}
+const Code = () => {
+  const snap = useSnapshot(state)
+  return (
+    <div>
+      <h2>Store Code</h2>
+      <pre>
+        {`import {proxy, useSnapshot} from 'valtio'
+const state = proxy({
+  dur: 4,
+  config: {
+    count: 1,
+    info: 'count',
+  },
+  incDur() {
+    ++state.dur
+  },
+  decDur() {
+    --state.dur
+  },
+  async setCountAsync() {
+    await waitAsync(500)
+    state.config.count++
+  },
+})`}
+      </pre>
+    </div>
+  )
+}
+const ShowInfo = () => {
+  const snap = useSnapshot(state)
+  return (
+    <h3>
+      Count Info {snap.config.info} <Refresh />
+    </h3>
+  )
+}
+const ShowCount = () => {
+  const snap = useSnapshot(state)
+  return (
+    <h2>
+      Snap Count: {snap.config.count} <Refresh />
+    </h2>
   )
 }
 const App = () => {
-  const snap = useSnapshot(state)
-
   return (
     <div>
+      <h1>
+        Valtio Store{' '}
+        <a
+          target="_blank"
+          href="https://github.com/ckken/emp-zustand/blob/main/projects/emp-valtio/src/App.tsx"
+          rel="noreferrer"
+        >
+          GitHub Code
+        </a>
+        <Refresh>TIM </Refresh>
+      </h1>
+      <ShowInfo />
       <ShowwDur />
-      <button onClick={decDur}>-</button>
-      <button onClick={incDur}>+</button>
-      <Refresh />
+      <ShowCount />
+      <Control />
+      <Code />
     </div>
   )
 }
--- projects/emp-valtio/src/Refresh.tsx
+++ projects/emp-valtio/src/Refresh.tsx
@@ -1,1 +1,6 @@
-export const Refresh = () => <span className="refresh">{Math.round(Math.random() * 1000)}</span>
+export const Refresh = ({children}: any) => (
+  <span className="refresh">
+    {children}
+    {Math.round(Math.random() * 1000)}
+  </span>
+)
