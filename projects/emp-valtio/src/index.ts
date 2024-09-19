cfdf3d56127e994ab4548f4be6d0c3e8469dde3d
chenweihao <chenweihao@yy.com>
2024-09-19 10:54:41 +0800

    fix: 补充polyfill

--- projects/emp-valtio/src/index.ts
+++ projects/emp-valtio/src/index.ts
@@ -1,4 +1,6 @@
 import 'core-js/features/object/get-own-property-descriptors'
 import 'core-js/features/object/entries'
+import 'core-js/features/object/values'
+// import 'core-js/es' // 导入 ES8 所有相关的 polyfills
 import './style.scss'
 import('./bootstrap')
